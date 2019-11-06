4b16e89c749e8b71426e8c24e962208fc60d28f3
hanjiajun <nujaijnah@live.com>
2019-11-06 17:02:57 +0800

    'input'

---
+++ react-block/src/components/input/input.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { omit } from '../_utils/utils'
+
+const Input = () => {
+    const inputNode = (
+        <input type="text"/>
+    )
+
+    return inputNode
+}
+
+Input.defaultProps  = {
+    
+}
+
+Input.propTypes = {
+
+}
